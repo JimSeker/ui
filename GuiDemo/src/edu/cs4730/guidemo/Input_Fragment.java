2d51ce192e2db7e776579435f5f76a7d80dcb4c5
JimSeker <seker@uwyo.edu>
2014-03-21 15:37:33 -0600

    try again

--- GuiDemo/src/edu/cs4730/guidemo/Input_Fragment.java
+++ GuiDemo/src/edu/cs4730/guidemo/Input_Fragment.java
@@ -57,7 +57,7 @@ public class Input_Fragment extends Fragment implements OnClickListener {
 
 			@Override
 			public void onTextChanged(CharSequence s, int start, int before, int count) {
-				// do nothing here.
+				// do nothing here. 
 				
 			}
 
