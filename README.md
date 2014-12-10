a59efd1a5828a1ab3922817deba45de2c031e5f1
JimSeker <seker@uwyo.edu>
2014-12-10 14:21:53 -0700

    being updates to android studio.
    Everything was moved into eclipse directory to prep for converting to
    android studio projects.

--- README.md
+++ README.md
@@ -3,6 +3,12 @@ GUI/UI Examples
 
 Varying examples of UI code for the interface.
 
+These projects are begin converted into Android studio.  For now, look in the eclipse/ directory for the UI examples.
+As the projects are converted, they will show up in the main directory.
+
+
+eclipse/  has the projects in the format that eclipse can read.
+
 GUIDemo is a series of example using a viewpager and fragments to show different widgets.
 
 NavDrawer (and android-support-v5-appcompat library) show the drawer with new arrow for lollipop.
---
+++ eclipse/GuiDemo/src/edu/cs4730/guidemo/MainActivity.java
@@ -0,0 +1,105 @@
+package edu.cs4730.guidemo;
+
+import java.util.Locale;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+
+
+public class MainActivity extends FragmentActivity {
+
+	String TAG = "MainActivity";
+	/**
+	 * The {@link android.support.v4.view.PagerAdapter} that will provide
+	 * fragments for each of the sections. We use a {@link FragmentPagerAdapter}
+	 * derivative, which will keep every loaded fragment in memory. If this
+	 * becomes too memory intensive, it may be best to switch to a
+	 * {@link android.support.v13.app.FragmentStatePagerAdapter}.
+	 */
+	SectionsPagerAdapter mSectionsPagerAdapter;
+
+	/**
+	 * The {@link ViewPager} that will host the section contents.
+	 */
+	ViewPager mViewPager;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		// Create the adapter that will return a fragment for each of the three
+		// primary sections of the activity.
+		mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+		// Set up the ViewPager with the sections adapter.
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mViewPager.setAdapter(mSectionsPagerAdapter);
+		//mViewPager.setCurrentItem(7);// set to a specific page in the pager.
+
+	}
+
+
+	/**
+	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+	 * one of the sections/tabs/pages.
+	 */
+	public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+		public SectionsPagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			switch (position) {
+			  case 0: return new Text_Fragment();
+			  case 1: return new Input_Fragment();
+			  case 2: return new Image_Fragment();
+			  case 3: return new Button_Fragment();
+			  case 4: return new Relativelayout_Fragment();
+			  case 5: return new RadioCheck_Fragment();
+			  case 6: return new Spinner_Fragment();
+			  case 7: return new ViewSwitch_Fragment();
+			  default: return null;
+			}
+		}
+
+		@Override
+		public int getCount() {
+			// Show X total pages.
+			return 8;
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			Locale l = Locale.getDefault();
+			switch (position) {
+			case 0:
+				return getString(R.string.title_section1).toUpperCase(l);
+			case 1:
+				return getString(R.string.title_section2).toUpperCase(l);
+			case 2:
+				return getString(R.string.title_section3).toUpperCase(l);
+			case 3:
+				return getString(R.string.title_section4).toUpperCase(l);
+			case 4:
+				return getString(R.string.title_section5).toUpperCase(l);
+			case 5:
+				return getString(R.string.title_section6).toUpperCase(l);
+			case 6:
+				return getString(R.string.title_section7).toUpperCase(l);
+			case 7:
+				return getString(R.string.title_section8).toUpperCase(l);
+
+			}
+			return null;
+		}
+	}
+
+
+}
