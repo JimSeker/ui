f8be4b4628dc9bf5fde2d9d9ea9d17cdabb8117e
JimSeker <seker@uwyo.edu>
2015-01-15 11:11:36 -0700

    updated to use NavDrawer, instead of viewpager.

--- GuiDemo/app/src/main/java/edu/cs4730/guidemo/MainActivity.java
+++ GuiDemo/app/src/main/java/edu/cs4730/guidemo/MainActivity.java
@@ -2,104 +2,151 @@ package edu.cs4730.guidemo;
 
 import java.util.Locale;
 
+import android.content.res.Configuration;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class MainActivity extends ActionBarActivity {
+
+    String TAG = "MainActivity";
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+    FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        fragmentManager = getSupportFragmentManager();
+
+        //first setup the listview with some simple categories via an array.
+        String[] values = getResources().getStringArray(R.array.sections);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, values);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerList.setAdapter(adapter);
+        mDrawerList.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View view, int position, long index) {
+                //based on click, change to correct fragment.
+                String item = mDrawerList.getAdapter().getItem(position).toString();
+                switch (position) {
+                    case 0:
+                        fragmentManager.beginTransaction().replace(R.id.container, new Text_Fragment()).commit();
+                        break;
+                    case 1:
+                        fragmentManager.beginTransaction().replace(R.id.container, new Input_Fragment()).commit();
+                        break;
+                    case 2:
+                        fragmentManager.beginTransaction().replace(R.id.container, new Image_Fragment()).commit();
+                        break;
+                    case 3:
+                        fragmentManager.beginTransaction().replace(R.id.container, new Button_Fragment()).commit();
+                        break;
+                    case 4:
+                        fragmentManager.beginTransaction().replace(R.id.container, new Relativelayout_Fragment()).commit();
+                        break;
+                    case 5:
+                        fragmentManager.beginTransaction().replace(R.id.container, new RadioCheck_Fragment()).commit();
+                        break;
+                    case 6:
+                        fragmentManager.beginTransaction().replace(R.id.container, new Spinner_Fragment()).commit();
+                        break;
+                    case 7:
+                        fragmentManager.beginTransaction().replace(R.id.container, new ViewSwitch_Fragment()).commit();
+                        break;
+                }
+                // update selected item and title, then close the drawer
+                mDrawerList.setItemChecked(position, true);
+                //now close the drawer!
+                mDrawerLayout.closeDrawer(mDrawerList);
+            }
+        });
 
 
-public class MainActivity extends FragmentActivity {
+        // enable ActionBar app icon to behave as action to toggle nav drawer
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
-	String TAG = "MainActivity";
-	/**
-	 * The {@link android.support.v4.view.PagerAdapter} that will provide
-	 * fragments for each of the sections. We use a {@link FragmentPagerAdapter}
-	 * derivative, which will keep every loaded fragment in memory. If this
-	 * becomes too memory intensive, it may be best to switch to a
-	 * {@link android.support.v13.app.FragmentStatePagerAdapter}.
-	 */
-	SectionsPagerAdapter mSectionsPagerAdapter;
+        // ActionBarDrawerToggle ties together the the proper interactions
+        // between the sliding drawer and the action bar app icon
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this, /* host Activity */
+                mDrawerLayout, /* DrawerLayout object */
+                R.string.drawer_open, /* "open drawer" description for accessibility */
+                R.string.drawer_close /* "close drawer" description for accessibility */
+        ) {
+            public void onDrawerClosed(View view) {
+                getSupportActionBar().setTitle(R.string.app_name);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
 
-	/**
-	 * The {@link ViewPager} that will host the section contents.
-	 */
-	ViewPager mViewPager;
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle("Categories");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        //first instance, so the default is zero.
+        fragmentManager.beginTransaction().replace(R.id.container, new Text_Fragment()).commit();
+        mDrawerList.setItemChecked(0, true);
+    }
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-		// Create the adapter that will return a fragment for each of the three
-		// primary sections of the activity.
-		mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        // ActionBarDrawerToggle will take care of this.
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-		// Set up the ViewPager with the sections adapter.
-		mViewPager = (ViewPager) findViewById(R.id.pager);
-		mViewPager.setAdapter(mSectionsPagerAdapter);
-		//mViewPager.setCurrentItem(7);// set to a specific page in the pager.
 
-	}
+    //needed to make the nav drawer draw correctly.
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
 
-
-	/**
-	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-	 * one of the sections/tabs/pages.
-	 */
-	public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-		public SectionsPagerAdapter(FragmentManager fm) {
-			super(fm);
-		}
-
-		@Override
-		public Fragment getItem(int position) {
-			switch (position) {
-			  case 0: return new Text_Fragment();
-			  case 1: return new Input_Fragment();
-			  case 2: return new Image_Fragment();
-			  case 3: return new Button_Fragment();
-			  case 4: return new Relativelayout_Fragment();
-			  case 5: return new RadioCheck_Fragment();
-			  case 6: return new Spinner_Fragment();
-			  case 7: return new ViewSwitch_Fragment();
-			  default: return null;
-			}
-		}
-
-		@Override
-		public int getCount() {
-			// Show X total pages.
-			return 8;
-		}
-
-		@Override
-		public CharSequence getPageTitle(int position) {
-			Locale l = Locale.getDefault();
-			switch (position) {
-			case 0:
-				return getString(R.string.title_section1).toUpperCase(l);
-			case 1:
-				return getString(R.string.title_section2).toUpperCase(l);
-			case 2:
-				return getString(R.string.title_section3).toUpperCase(l);
-			case 3:
-				return getString(R.string.title_section4).toUpperCase(l);
-			case 4:
-				return getString(R.string.title_section5).toUpperCase(l);
-			case 5:
-				return getString(R.string.title_section6).toUpperCase(l);
-			case 6:
-				return getString(R.string.title_section7).toUpperCase(l);
-			case 7:
-				return getString(R.string.title_section8).toUpperCase(l);
-
-			}
-			return null;
-		}
-	}
-
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
 
 }
