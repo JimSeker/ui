88fd6761338ade2ae20f2f6ff1abf5faa4170769
JimSeker <seker@uwyo.edu>
2015-01-20 22:45:33 -0700

    add communication examples.

---
+++ Communication/simpleFragComDemo/app/src/main/java/edu/cs4730/simplefragcomdemo/MainFragment.java
@@ -0,0 +1,75 @@
+package edu.cs4730.simplefragcomdemo;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MainFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ */
+public class MainFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+    Button myButton;
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View myView = inflater.inflate(R.layout.fragment_main, container, false);
+        myButton = (Button) myView.findViewById(R.id.button01);
+        myButton.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   if (mListener != null) {
+                       mListener.onFragmentInteraction(1);
+                   }
+               }
+        });
+
+        return myView;
+    }
+
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     */
+    public interface OnFragmentInteractionListener {
+        public void onFragmentInteraction(Integer Num);
+    }
+
+}
