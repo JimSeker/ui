9d4741254eceba963d1b6fac3f1281c7051a2b12
JimSeker <seker@uwyo.edu>
2015-02-06 14:54:09 -0700

    fixed the listview demos.

--- ListViews/ListviewFragmentDemo/app/src/main/java/edu/cs4730/listviewfragmentdemo/MainActivity.java
+++ ListViews/ListviewFragmentDemo/app/src/main/java/edu/cs4730/listviewfragmentdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.cs4730.listviewfragmentdemo;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,18 +8,35 @@ import android.view.MenuItem;
 
 /*
  * This is an example using the listview in a fragment.  There is very little code here that is not default
- * except the callbacks for the fragment named titlefrag.
+ * except the callbacks for the fragment named titlefrag.  There is a layout and layout-land for this
+ * so the code also decides if it needs to display a fragment or if it is already showing.
  *
  * see the two fragments textFrag and titlefrag for the bulk of the code.
  */
 
 public class MainActivity extends ActionBarActivity implements titlefrag.Callbacks {
+    boolean TwoPane = false;
+
+
+    textFrag myTextFrag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        if (findViewById(R.id.container) == null ) {
+        //landscape or large mode. both fragments will be displayed on the screen.
+            //nothing to do, since it already showing.
+            TwoPane = true;
+        } else {
+            TwoPane = false;
+            //add the title fragment.
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new titlefrag())
+                    .commit();
+            myTextFrag = new textFrag();  //needed for later.
+        }
+     }
 
 
     @Override
@@ -44,7 +62,22 @@ public class MainActivity extends ActionBarActivity implements titlefrag.Callbac
     }
     @Override
     public void onItemSelected(int id) {
-        ((textFrag) getSupportFragmentManager().findFragmentById(R.id.frag_text)
-        ).setText(id);
+        if (TwoPane) {
+            //already showing, so just update it.
+            ((textFrag) getSupportFragmentManager().findFragmentById(R.id.frag_text)
+            ).setText(id);
+        }else {
+            //now add the arg's and startup the textfragment.
+            Bundle args = new Bundle();
+            args.putInt("position", id);
+            myTextFrag.setArguments(args);
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            // Replace whatever is in the fragment_container view with this fragment,
+            // and add the transaction to the back stack so the user can navigate back
+            transaction.replace(R.id.container, myTextFrag, "second" );
+            transaction.addToBackStack(null);
+            // Commit the transaction
+            transaction.commit();
+        }
     }
 }
