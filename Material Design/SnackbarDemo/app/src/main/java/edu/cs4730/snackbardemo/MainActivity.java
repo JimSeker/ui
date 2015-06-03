06877821ed4b8bb51fe639788d001d66b2540b93
JimSeker <seker@uwyo.edu>
2015-06-03 11:39:32 -0600

    initial example of support design

---
+++ Material Design/SnackbarDemo/app/src/main/java/edu/cs4730/snackbardemo/MainActivity.java
@@ -0,0 +1,142 @@
+package edu.cs4730.snackbardemo;
+
+import android.content.res.Configuration;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+//http://android-developers.blogspot.com/2015/05/android-design-support-library.html
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerlayout;
+    private NavigationView mNavigationView;
+    FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //use the v7.toolbar instead of the default one.
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+        // enable ActionBar app icon to behave as action to toggle nav drawer
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mDrawerlayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this,  // host activity
+                mDrawerlayout,  //drawerlayout object
+                toolbar,  //toolbar
+                R.string.drawer_open,  //open drawer description  required!
+                R.string.drawer_close) {  //closed drawer description
+
+            //called once the drawer has closed.
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Categories");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            //called when the drawer is now open.
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getSupportActionBar().setTitle(R.string.app_name);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        //To disable the icon for the drawer, change this to false
+        //mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerlayout.setDrawerListener(mDrawerToggle);
+
+        mNavigationView = (NavigationView) findViewById(R.id.navview);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                if (id == R.id.navigation_item_1) {
+                    //load fragment
+                    if (!menuItem.isChecked()) {  //only need to do this if fragment is already loaded.
+                        menuItem.setChecked(true);
+                        fragmentManager.beginTransaction().replace(R.id.container, new SnackBarFragment()).commit();
+                    }
+                    mDrawerlayout.closeDrawers();
+                    return true;
+                } else if (id == R.id.navigation_item_3) {
+                    //load fragment
+                    if (!menuItem.isChecked()) {  //only need to do this if fragment is already loaded.
+                        menuItem.setChecked(true);
+                        fragmentManager.beginTransaction().replace(R.id.container, new SB_FABFragment()).commit();
+                    }
+
+                    mDrawerlayout.closeDrawers();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        //finally deal with the fragments.
+        fragmentManager = getSupportFragmentManager();
+        //first instance, so the default is zero.
+        fragmentManager.beginTransaction().replace(R.id.container, new SnackBarFragment()).commit();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * When using the ActionBarDrawerToggle, you must call it during
+     * onPostCreate() and onConfigurationChanged()...
+     */
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+}
---
+++ Material Design/SnackbarDemo/app/src/main/java/edu/cs4730/snackbardemo/SnackBarFragment.java
@@ -0,0 +1,48 @@
+package edu.cs4730.snackbardemo;
+
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SnackBarFragment extends Fragment implements  View.OnClickListener{
+
+    Button btn;
+    View myView;
+
+    public SnackBarFragment() {
+    }
+    View.OnClickListener SBonClickListener = new View.OnClickListener(){
+        public void  onClick  (View  v){
+            Toast.makeText(getActivity(),"You clicked undo", Toast.LENGTH_LONG).show();
+        }
+    };
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.fragment_snackbar, container, false);
+        btn = (Button) myView.findViewById(R.id.button01);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(myView, "Hi there?", Snackbar.LENGTH_LONG)
+                        .setAction("Undo?", SBonClickListener)
+                        .show();
+            }
+        });
+        return myView;
+    }
+
+    //click listener for the snackbar button.
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(getActivity(),"You clicked undo", Toast.LENGTH_LONG).show();
+    }
+}
