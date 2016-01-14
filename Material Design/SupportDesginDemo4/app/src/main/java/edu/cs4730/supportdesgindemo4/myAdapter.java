317af70d6b44b16ebcc3af58c23c18f2721c3283
JimSeker <seker@uwyo.edu>
2016-01-14 13:54:43 -0700

    added new example, renamed an example.
    supportDesign3 was renamed supportDesignDemo4

---
+++ Material Design/SupportDesginDemo4/app/src/main/java/edu/cs4730/supportdesgindemo4/myAdapter.java
@@ -0,0 +1,67 @@
+package edu.cs4730.supportdesgindemo4;
+
+/**
+ * Created by Seker on 1/22/2015.
+ */
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+/*
+ * this adapter is very similar to the adapters used for listview, except a ViewHolder is required
+ * see http://developer.android.com/training/improving-layouts/smooth-scrolling.html
+ * except instead having to implement a ViewHolder, it is implemented within
+ * the adapter.
+ */
+
+public class myAdapter extends RecyclerView.Adapter<myAdapter.ViewHolder>{
+
+    private List<String> myList;
+    private int rowLayout;
+    private Context mContext;
+
+    public myAdapter(List<String> myList, int rowLayout, Context context) {
+        this.myList = myList;
+        this.rowLayout = rowLayout;
+        this.mContext = context;
+    }
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(rowLayout, viewGroup, false);
+        return new ViewHolder(v);
+    }
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+        String entry = myList.get(i);
+        viewHolder.myName.setText(entry);
+        viewHolder.myName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView tv = (TextView)v;
+                Toast.makeText(mContext,tv.getText(),Toast.LENGTH_SHORT).show();
+            }
+        });
+        viewHolder.Pic.setImageResource(R.drawable.phone);
+    }  //mContext.getDrawable(country.getImageResourceId(mContext))
+    @Override
+    public int getItemCount() {
+        return myList == null ? 0 : myList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView myName;
+        public ImageView Pic;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            myName = (TextView) itemView.findViewById(R.id.Name);
+            Pic= (ImageView)itemView.findViewById(R.id.picture);
+        }
+    }
+}
---
+++ Material Design/SupportDesignDemo3/app/src/main/java/edu/cs4730/supportdesigndemo3/MainActivity.java
@@ -0,0 +1,108 @@
+package edu.cs4730.supportdesigndemo3;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/*
+ * example mostly shows a collapsing toolbar, which is in the xml
+ * it also changes the status and toolbar color as well, plus the text size as the toolbar is collapsing.
+ *
+ *
+ *  Some code from http://www.tutorialsbuzz.com/2015/11/android-collapsingtoolbarlayout-example_7.html
+ * and http://antonioleiva.com/collapsing-toolbar-layout/
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    private CollapsingToolbarLayout collapsingToolbarLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle(getResources().getString(R.string.app_name));
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        dynamicToolbarColor();
+
+        toolbarTextAppernce();
+    }
+
+
+    private void dynamicToolbarColor() {
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
+                R.drawable.wolf_gray);
+        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+            @Override
+            public void onGenerated(Palette palette) {
+
+                //a dark color  a dark blue
+                collapsingToolbarLayout.setContentScrimColor(palette.getDarkVibrantColor(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary)));
+                //a muted color   mostly gray
+                //collapsingToolbarLayout.setContentScrimColor(palette.getMutedColor(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary)));
+                //dark muted color  very dark blue, almost black.
+               // collapsingToolbarLayout.setContentScrimColor(palette.getDarkMutedColor(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary)));
+                //very bright color
+                //collapsingToolbarLayout.setContentScrimColor(palette.getLightVibrantColor(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary)));
+                //this one doesn't seem to do anything.
+                //collapsingToolbarLayout.setStatusBarScrimColor(palette.getDarkMutedColor(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimaryDark)));
+
+            }
+        });
+    }
+
+
+    private void toolbarTextAppernce() {
+        collapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.collapsedappbar);
+        collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.expandedappbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
