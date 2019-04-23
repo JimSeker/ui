e164eb6827a834d13266742a6b137fcacb397052
Jim Ward <seker@uwyo.edu>
2019-04-23 13:43:50 -0600

    added third example.

--- flutter/README.md
+++ flutter/README.md
@@ -6,6 +6,7 @@ These are examples very basic examples using flutter.
 
 <b>flutter_formexample2</b> is an attempt to redo the formexample in https://github.com/JimSeker/ui/tree/master/Basic 
 
+<b>flutter_button</b> is simple statefulwidget example of using a button and changing text 
 
 These are example code for University of Wyoming, Cosc 4730 Mobile Programming course.
 All examples are for Android, but in flutter could be used on an Ios device.
---
+++ flutter/flutter_button/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SampleAppPage(),
+    );
+  }
+}
+
+class SampleAppPage extends StatefulWidget {
+  SampleAppPage({Key key}) : super(key: key);
+
+  @override
+  _SampleAppPageState createState() => _SampleAppPageState();
+}
+
+class _SampleAppPageState extends State<SampleAppPage> {
+  // Default placeholder text
+  String textToShow = "Flutter is interesting.";
+
+  void _updateText() {
+    setState(() {
+      // update the text
+      textToShow = "Flutter has a weird state!";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sample App"),
+      ),
+      body: Center(child: Text(textToShow)),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _updateText,
+        tooltip: 'Update Text',
+        child: Icon(Icons.update),
+      ),
+    );
+  }
+}
