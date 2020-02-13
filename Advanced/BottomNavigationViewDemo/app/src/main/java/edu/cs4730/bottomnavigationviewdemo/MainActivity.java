ea1effbe29f3b535556b01046d548e3af20ff972
Jim Ward <seker@uwyo.edu>
2020-02-13 15:19:49 -0700

    added new example

---
+++ Advanced/BottomNavigationViewDemo/app/src/main/java/edu/cs4730/bottomnavigationviewdemo/MainActivity.java
@@ -0,0 +1,50 @@
+package edu.cs4730.bottomnavigationviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+
+        /*  if not using arch navigation, then you need to implement this.
+        navView.setOnNavigationItemSelectedListener(
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                public boolean onNavigationItemSelected(MenuItem item) {
+                    return false;
+                }
+            }
+
+        );
+        */
+
+        // Passing each menu ID as a set of Ids because each menu should be considered as top level destinations.
+        //Note for this to work with arch Navigation, these id must be the same id in menu.xml and the nav_graph.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+            R.id.action_first, R.id.action_second, R.id.action_third)
+            .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+
+        //In order to have badges, you need to use the Theme.MaterialComponents.DayNight  (doesn't have to daynight, but materialcompents).
+        BadgeDrawable badge = navView.getOrCreateBadge(R.id.action_second);
+        badge.setNumber(12);  //should show a 12 in the "badge" for the second one.
+        badge.setVisible(true);
+
+
+    }
+}
---
+++ Advanced/BottomNavigationViewDemo/app/src/main/java/edu/cs4730/bottomnavigationviewdemo/threeFragment.java
@@ -0,0 +1,31 @@
+package edu.cs4730.bottomnavigationviewdemo;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class threeFragment extends Fragment {
+
+
+    public threeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_three, container, false);
+    }
+
+}
