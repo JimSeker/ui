bd1359fa5bba24a799f4629382a632625e150caf
Jim Ward <seker@uwyo.edu>
2020-04-22 10:45:46 -0600

    fixed the test import

--- flutter/navigation_drawer_demo/test/widget_test.dart
+++ flutter/navigation_drawer_demo/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:navigationdrawerdemo/main.dart';
+import 'package:navigation_drawer_demo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
