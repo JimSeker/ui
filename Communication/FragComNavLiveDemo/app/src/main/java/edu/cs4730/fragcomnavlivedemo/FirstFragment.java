1ac94e3cb8d599c80869e1e0e117de6879cc9e56
Jim Ward <seker@uwyo.edu>
2020-09-18 10:48:12 -0600

    fixed the modelview.

--- Communication/FragComNavLiveDemo/app/src/main/java/edu/cs4730/fragcomnavlivedemo/FirstFragment.java
+++ Communication/FragComNavLiveDemo/app/src/main/java/edu/cs4730/fragcomnavlivedemo/FirstFragment.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.Navigation;
 
@@ -36,7 +37,7 @@ public class FirstFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View myView = inflater.inflate(R.layout.fragment_first, container, false);
-        mViewModel = ViewModelProviders.of(getActivity()).get(DataViewModel.class);
+        mViewModel = new ViewModelProvider(getActivity()).get(DataViewModel.class);
         mViewModel.getoneLD().observe(this, new Observer<Integer>() {
             @Override
             public void onChanged(@Nullable Integer v) {
