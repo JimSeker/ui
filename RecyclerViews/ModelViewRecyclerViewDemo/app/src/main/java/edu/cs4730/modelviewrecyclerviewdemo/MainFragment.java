038cf0095b36ad202fac9b35861ebd75a2d4246d
Jim Ward <seker@uwyo.edu>
2021-09-14 09:31:26 -0600

    updated to API 31, added required export.

--- RecyclerViews/ModelViewRecyclerViewDemo/app/src/main/java/edu/cs4730/modelviewrecyclerviewdemo/MainFragment.java
+++ RecyclerViews/ModelViewRecyclerViewDemo/app/src/main/java/edu/cs4730/modelviewrecyclerviewdemo/MainFragment.java
@@ -14,7 +14,6 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
