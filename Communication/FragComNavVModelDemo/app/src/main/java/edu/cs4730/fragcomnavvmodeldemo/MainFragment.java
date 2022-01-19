d308108d5c54c7188b24c989b0a3520efc2c2d3d
Jim Ward <seker@uwyo.edu>
2022-01-19 11:03:21 -0700

    fixes for the viewmodel.

--- Communication/FragComNavVModelDemo/app/src/main/java/edu/cs4730/fragcomnavvmodeldemo/MainFragment.java
+++ Communication/FragComNavVModelDemo/app/src/main/java/edu/cs4730/fragcomnavvmodeldemo/MainFragment.java
@@ -10,7 +10,6 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.Navigation;
 
 /**
