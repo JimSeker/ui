13f2064b0d16be2a9b60c1c9179d806b14961e43
Jim Ward <seker@uwyo.edu>
2022-02-14 08:31:48 -0700

    clean up and fixes.

--- Material Design/CirToolbarNavDrawerDemo/app/src/main/java/edu/cs4730/cirtoolbarnavdrawerdemo/MainActivity.java
+++ Material Design/CirToolbarNavDrawerDemo/app/src/main/java/edu/cs4730/cirtoolbarnavdrawerdemo/MainActivity.java
@@ -1,12 +1,18 @@
 package edu.cs4730.cirtoolbarnavdrawerdemo;
 
 import android.content.res.Configuration;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import androidx.annotation.NonNull;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+
 import android.os.Bundle;
+
 import androidx.appcompat.widget.Toolbar;
+
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,10 +22,10 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-/*
- *  This is the same as ToolbarNavDrawer example.  The support design library has been added.
- *  so that the floatingActionButton can be used.  it would go to API 7+, this is set to 14 for fonts style
- *  used in the NavDrawer layout.  Not attempt at animation was made in this example for the FAB.
+/**
+ * This is the same as ToolbarNavDrawer example in the navigation directory.  The support design
+ * library has been added. so that the floatingActionButton can be used.
+ * No attempt at animation was made in this example for the FAB.
  */
 
 public class MainActivity extends AppCompatActivity {
@@ -35,23 +41,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //use the v7.toolbar instead of the default one.
-        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        //use the androidx... .toolbar instead of the actionbar, which is disabled in the style.
+        toolbar = findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
-        mTextView = (TextView) findViewById(R.id.textview01);
+        mTextView = findViewById(R.id.textview01);
+        mDrawerlayout = findViewById(R.id.drawer_layout);
+
+        //lastly setup the listview with some simple categories via an array.
+        String[] values = new String[]{"Item 1", "Item 2", "Item 3", "Item 4"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+            R.layout.drawer_list_item, values);
+        mDrawerList =  findViewById(R.id.left_drawer);
+        mDrawerList.setAdapter(adapter);
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View view, int position, long index) {
+                //yes, this should do something for more interesting.  but this is an example.
+                String item = mDrawerList.getAdapter().getItem(position).toString();
+                mTextView.setText(item);
+                // update selected item and title, then close the drawer
+                mDrawerList.setItemChecked(position, true);
+                //now close the drawer!
+                mDrawerlayout.closeDrawers();
+
+            }
+        });
+        //set the default value and position to checked.
+        mDrawerList.setItemChecked(0, true);
+        mTextView.setText(values[0]);
 
         // enable ActionBar app icon to behave as action to toggle nav drawer
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
-        mDrawerlayout = (DrawerLayout)findViewById(R.id.drawer_layout);
-
         mDrawerToggle = new ActionBarDrawerToggle(this,  // host activity
-                mDrawerlayout,  //drawerlayout object
-                toolbar,  //toolbar
-                R.string.drawer_open,  //open drawer description  required!
-                R.string.drawer_close) {  //closed drawer description
+            mDrawerlayout,  //drawerlayout object
+            toolbar,  //toolbar
+            R.string.drawer_open,  //open drawer description  required!
+            R.string.drawer_close) {  //closed drawer description
 
             //called once the drawer has closed.
             @Override
@@ -73,33 +101,11 @@ public class MainActivity extends AppCompatActivity {
         //mDrawerToggle.setDrawerIndicatorEnabled(true);
         mDrawerlayout.addDrawerListener(mDrawerToggle);
 
-        //lastly setup the listview with some simple categories via an array.
-        String[] values = new String[] { "Item 1", "Item 2", "Item 3", "Item 4" };
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                R.layout.drawer_list_item, values);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-        mDrawerList.setAdapter(adapter);
-        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View view, int position, long index) {
-                //yes, this should do something for more interesting.  but this is an example.
-                String item = mDrawerList.getAdapter().getItem(position).toString();
-                mTextView.setText(item);
-                // update selected item and title, then close the drawer
-                mDrawerList.setItemChecked(position, true);
-                //now close the drawer!
-                mDrawerlayout.closeDrawers();
-
-            }
-        });
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setRippleColor(getResources().getColor(R.color.rippleColor));  //set this changes it color to the ripple color.  mistake in the their code?
-        //without it, the ripple code does work correctly.  odder.
-
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getBaseContext(),"You clicked it!", Toast.LENGTH_LONG).show();
+                Toast.makeText(getBaseContext(), "You clicked it!", Toast.LENGTH_LONG).show();
             }
         });
 
@@ -141,7 +147,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig) {
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         // Pass any configuration change to the drawer toggls
         mDrawerToggle.onConfigurationChanged(newConfig);
