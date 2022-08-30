b8f36b50a005481c3a772a099d9e979537b6fd7c
Jim Ward <seker@uwyo.edu>
2022-08-30 10:39:18 -0600

    updated to API 33 and now uses bottomNavView.

--- ListViews/EsplistviewDemo/app/src/main/java/edu/cs4730/esplistviewdemo/MainActivity.java
+++ ListViews/EsplistviewDemo/app/src/main/java/edu/cs4730/esplistviewdemo/MainActivity.java
@@ -1,80 +1,55 @@
 package edu.cs4730.esplistviewdemo;
 
-import java.util.Locale;
-
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
 
+/**
+ *  Demo of how to use the expandable listviews.  code is mostly the fragments.
+ */
 
 public class MainActivity extends AppCompatActivity {
 
     String TAG = "MainActivity";
 
-    SectionsPagerAdapter mSectionsPagerAdapter;
-    ViewPager mViewPager;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
 
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        navView.setOnItemSelectedListener(
+            new BottomNavigationView.OnItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    //setup the fragments here.
+                    int id = item.getItemId();
+                    if (id == R.id.action_first) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, new elvDemo1_Fragment()).commit();
+                        item.setChecked(true);
+                        return true;
+                    } else if (id == R.id.action_second) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, new elvDemo2_Fragment()).commit();
+                        item.setChecked(true);
+                        return true;
+                    }
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-        //mViewPager.setCurrentItem(7);// set to a specific page in the pager.
+                    return false;
+                }
+
+            }
+
+        );
+        //start it with the first fragment, if we just started the app.
+        if (savedInstanceState == null)
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, new elvDemo1_Fragment()).commit();
 
 
     }
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            switch (position) {
-                case 0:
-                    return new elvDemo1_Fragment();
-                case 1:
-                    return new elvDemo2_Fragment();
-
-                default:
-                    return null;
-            }
-        }
-
-        @Override
-        public int getCount() {
-            // Show X total pages.
-            return 2;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            Locale l = Locale.getDefault();
-            switch (position) {
-                case 0:
-                    return getString(R.string.title_section1).toUpperCase(l);
-                case 1:
-                    return getString(R.string.title_section2).toUpperCase(l);
-            }
-            return null;
-        }
-    }
 
 }
--- ListViews/EsplistviewDemo/app/src/main/java/edu/cs4730/esplistviewdemo/elvDemo1_Fragment.java
+++ ListViews/EsplistviewDemo/app/src/main/java/edu/cs4730/esplistviewdemo/elvDemo1_Fragment.java
@@ -3,109 +3,89 @@ package edu.cs4730.esplistviewdemo;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import android.content.Context;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.util.Log;
 import android.widget.ExpandableListView;
 
 import androidx.fragment.app.Fragment;
 
 public class elvDemo1_Fragment extends Fragment {
 
-	String TAG = "elvDemo1_Fragment";
-	Context myContext;
+    String TAG = "elvDemo1_Fragment";
 
-	ExpandableListAdapter listAdapter;
-	ExpandableListView expListView;
-	List<String> listDataHeader;
-	HashMap<String, List<String>> listDataChild;
-
-
-	public elvDemo1_Fragment() {
-		// Required empty public constructor
-	}
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		Log.d(TAG, "OnCreate");
-	}
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
-		View myView = inflater.inflate(R.layout.elvdemo1_fragment, container, false);
+    public elvDemo1_Fragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View myView = inflater.inflate(R.layout.elvdemo1_fragment, container, false);
+
+        // get the listview
+        expListView = myView.findViewById(R.id.lvExp);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(requireContext(), listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+        return myView;
+    }
 
 
-		// get the listview
-		expListView = (ExpandableListView) myView.findViewById(R.id.lvExp);
+    /**
+     * Preparing the list data
+     */
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
 
-		// preparing list data
-		prepareListData();
+        // Adding child data
+        listDataHeader.add("Top 250");
+        listDataHeader.add("Now Showing");
+        listDataHeader.add("Coming Soon..");
 
-		listAdapter = new ExpandableListAdapter(myContext, listDataHeader, listDataChild);
+        // Adding child data
+        List<String> top250 = new ArrayList<String>();
+        top250.add("The Shawshank Redemption");
+        top250.add("The Godfather");
+        top250.add("The Godfather: Part II");
+        top250.add("Pulp Fiction");
+        top250.add("The Good, the Bad and the Ugly");
+        top250.add("The Dark Knight");
+        top250.add("12 Angry Men");
 
-		// setting list adapter
-		expListView.setAdapter(listAdapter);
-		
-		return myView;
-	}
+        List<String> nowShowing = new ArrayList<String>();
+        nowShowing.add("The Conjuring");
+        nowShowing.add("Despicable Me 2");
+        nowShowing.add("Turbo");
+        nowShowing.add("Grown Ups 2");
+        nowShowing.add("Red 2");
+        nowShowing.add("The Wolverine");
 
+        List<String> comingSoon = new ArrayList<String>();
+        comingSoon.add("2 Guns");
+        comingSoon.add("The Smurfs 2");
+        comingSoon.add("The Spectacular Now");
+        comingSoon.add("The Canyons");
+        comingSoon.add("Europa Report");
 
-	/*
-	 * Preparing the list data
-	 */
-	private void prepareListData() {
-		listDataHeader = new ArrayList<String>();
-		listDataChild = new HashMap<String, List<String>>();
-
-		// Adding child data
-		listDataHeader.add("Top 250");
-		listDataHeader.add("Now Showing");
-		listDataHeader.add("Coming Soon..");
-
-		// Adding child data
-		List<String> top250 = new ArrayList<String>();
-		top250.add("The Shawshank Redemption");
-		top250.add("The Godfather");
-		top250.add("The Godfather: Part II");
-		top250.add("Pulp Fiction");
-		top250.add("The Good, the Bad and the Ugly");
-		top250.add("The Dark Knight");
-		top250.add("12 Angry Men");
-
-		List<String> nowShowing = new ArrayList<String>();
-		nowShowing.add("The Conjuring");
-		nowShowing.add("Despicable Me 2");
-		nowShowing.add("Turbo");
-		nowShowing.add("Grown Ups 2");
-		nowShowing.add("Red 2");
-		nowShowing.add("The Wolverine");
-
-		List<String> comingSoon = new ArrayList<String>();
-		comingSoon.add("2 Guns");
-		comingSoon.add("The Smurfs 2");
-		comingSoon.add("The Spectacular Now");
-		comingSoon.add("The Canyons");
-		comingSoon.add("Europa Report");
-
-		listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
-		listDataChild.put(listDataHeader.get(1), nowShowing);
-		listDataChild.put(listDataHeader.get(2), comingSoon);
-	}
-	
-	@Override
-	public void onAttach(Context context) {
-		super.onAttach(context);
-		myContext = context; //needed for toast.
-		Log.d(TAG,"onAttach");
-	}
-
-
+        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), nowShowing);
+        listDataChild.put(listDataHeader.get(2), comingSoon);
+    }
 
 }
