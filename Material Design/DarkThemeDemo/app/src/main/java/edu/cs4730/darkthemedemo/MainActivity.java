b2242468f4cb2f00ac04a47e4a94266b067837c7
Jim Ward <seker@uwyo.edu>
2022-09-13 13:52:05 -0600

    updated to match the kotlin version better.

--- Material Design/DarkThemeDemo/app/src/main/java/edu/cs4730/darkthemedemo/MainActivity.java
+++ Material Design/DarkThemeDemo/app/src/main/java/edu/cs4730/darkthemedemo/MainActivity.java
@@ -3,6 +3,7 @@ package edu.cs4730.darkthemedemo;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -19,17 +20,20 @@ import android.view.View;
  *
  * Need add the DayNight (both app and material?) stuff for AppCompatDelegate.setDefaultNightMode()
  *  with MODE_LIGHT_NO/YES and MODE_NIGHT_AUTO_BATTERY  System default - MODE_NIGHT_FOLLOW_SYSTEM
+ *
+ *  don't change the commit() to apply() or the app will crash a lot.  need to finish the commit before
+ *  the app restarts the drawing.
  */
 
 public class MainActivity extends AppCompatActivity {
     SharedPreferences preferences;
-    int theme;
-
+    int mytheme;
+    @SuppressLint("ApplySharedPref")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         preferences = getSharedPreferences("example", MODE_PRIVATE);
-        theme = preferences.getInt("Theme", R.style.AppTheme);
-        setTheme(theme);  //must be fore the super!
+        mytheme = preferences.getInt("Theme", R.style.AppTheme);
+        setTheme(mytheme);  //must be fore the super!
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
