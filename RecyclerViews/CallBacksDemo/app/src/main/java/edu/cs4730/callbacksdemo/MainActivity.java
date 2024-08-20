7c8476cf7bf1931731cd35cb5aff68e7e3238cad
Jim Ward <JimSeker@users.noreply.github.com>
2024-08-20 14:45:28 -0600

    updated to api 35, lots of gradle fixes.

--- RecyclerViews/CallBacksDemo/app/src/main/java/edu/cs4730/callbacksdemo/MainActivity.java
+++ RecyclerViews/CallBacksDemo/app/src/main/java/edu/cs4730/callbacksdemo/MainActivity.java
@@ -7,11 +7,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import edu.cs4730.callbacksdemo.databinding.ActivityMainBinding;
 
