91bf0f04effc13c67a4ba665368389708814f170
Jim Ward <JimSeker@users.noreply.github.com>
2025-07-28 10:52:20 -0600

    fixed deprecated calls.

--- Navigation/MenuDemo_kt/app/src/main/java/edu/cs4730/menudemo_kt/FragMenu1.kt
+++ Navigation/MenuDemo_kt/app/src/main/java/edu/cs4730/menudemo_kt/FragMenu1.kt
@@ -1,43 +1,44 @@
 package edu.cs4730.menudemo_kt
 
 import android.os.Bundle
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import edu.cs4730.menudemo_kt.databinding.FragmenuBinding
 
 class FragMenu1 : Fragment() {
     private lateinit var binding: FragmenuBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        //required if the fragment is adding menu items, otherwise it calls the menu methods.
-        setHasOptionsMenu(true)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        binding = FragmenuBinding.inflate(inflater, container, false);
+        binding = FragmenuBinding.inflate(inflater, container, false)
         binding.tv.text = "Fragment #1"
+
+
+        //add menu items to the fragment.
+        requireActivity().addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.frag1menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                if (menuItem.itemId == R.id.frag1item) {
+                    Toast.makeText(requireContext(), "Fragment #1", Toast.LENGTH_LONG).show()
+                    return true
+                }
+                return false
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
         return binding.root
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.frag1menu, menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.frag1item) {
-            Toast.makeText(requireContext(), "Fragment #1", Toast.LENGTH_LONG).show()
-            return true
-        }
-        return super.onOptionsItemSelected(item)
-    }
 }
--- Navigation/MenuDemo_kt/app/src/main/java/edu/cs4730/menudemo_kt/FragMenu2.kt
+++ Navigation/MenuDemo_kt/app/src/main/java/edu/cs4730/menudemo_kt/FragMenu2.kt
@@ -8,37 +8,35 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import edu.cs4730.menudemo_kt.databinding.FragmenuBinding
 
 class FragMenu2 : Fragment() {
     lateinit var binding: FragmenuBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        //required if the fragment is adding menu items, otherwise it calls the menu methods.
-        setHasOptionsMenu(true)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = FragmenuBinding.inflate(inflater, container, false)
         binding.tv.text = "Fragment #2"
+
+        //add menu items to the fragment.
+        requireActivity().addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.frag2menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                if (menuItem.itemId == R.id.frag2item) {
+                    Toast.makeText(requireContext(), "Fragment #1", Toast.LENGTH_LONG).show()
+                    return true
+                }
+                return false
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
         return binding.root
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.frag2menu, menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.frag2item) {
-            Toast.makeText(requireContext(), "Fragment #2", Toast.LENGTH_LONG)
-                .show()
-            return true
-        }
-        return super.onOptionsItemSelected(item)
-    }
 }
