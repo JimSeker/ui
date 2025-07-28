88b81f7e1f98b7a24ef6b37af592538769ddbfd1
Jim Ward <JimSeker@users.noreply.github.com>
2025-07-28 10:51:41 -0600

    fixed deprecated calls.

--- Navigation/MenuDemo/app/src/main/java/edu/cs4730/menudemo/FragMenu1.java
+++ Navigation/MenuDemo/app/src/main/java/edu/cs4730/menudemo/FragMenu1.java
@@ -1,7 +1,9 @@
 package edu.cs4730.menudemo;
 
 import androidx.annotation.NonNull;
+import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Lifecycle;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -21,33 +23,27 @@ public class FragMenu1 extends Fragment {
     FragmenuBinding binding;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //required if the fragment is adding menu items, otherwise it calls the menu methods.
-        setHasOptionsMenu(true);
-    }
-
-    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmenuBinding.inflate(inflater, container, false);
         binding.tv.setText("Fragment #1");
+
+        //add menu items to the fragment.
+        requireActivity().addMenuProvider(new MenuProvider() {
+            @Override
+            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+                menuInflater.inflate(R.menu.frag1menu, menu);
+            }
+
+            @Override
+            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.frag1item) {
+                    Toast.makeText(requireContext(), "Fragment #1", Toast.LENGTH_LONG).show();
+                    return true;
+                }
+                return false;
+            }
+        }, getViewLifecycleOwner(), Lifecycle.State.RESUMED);
+
         return binding.getRoot();
     }
-
-    @Override
-    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
-        inflater.inflate(R.menu.frag1menu, menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.frag1item) {
-            Toast.makeText(requireContext(), "Fragment #1", Toast.LENGTH_LONG).show();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-
 }
